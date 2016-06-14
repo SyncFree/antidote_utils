74211279bb82f90840452227b791522cae9fa135
tcrain <tyler.crain@lip6.fr>
2016-06-14 18:29:15 +0200

    fix atom conversion

--- src/json_utilities.erl
+++ src/json_utilities.erl
@@ -50,11 +50,11 @@ crdt_from_json([{orset,Value}]) ->
     crdt_orset:from_json([{orset,Value}]).
 
 txid_to_json(#tx_id{snapshot_time=Time,server_pid=Pid}) ->
-    [{txid,[convert_to_json(Time),atom_from_json(Pid)]}].
+    [{txid,[convert_to_json(Time),atom_to_json(Pid)]}].
 
 txid_from_json([{txid,[JTime,JPid]}]) ->
     #tx_id{snapshot_time=deconvert_from_json(JTime),
-	  server_pid=atom_to_json(JPid)}.
+	  server_pid=atom_from_json(JPid)}.
 
 pid_to_json(PID) ->
     [{pid,PID}].
