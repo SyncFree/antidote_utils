288d86a74a3126dc59e43bed098cef27e478c456
santialvarezcolombo <santialvarezcolombo@gmail.com>
2016-09-07 11:45:50 -0300

    Added the log_record, which will be used as the new value stored by antidoteDB.

--- include/antidote_utils.hrl
+++ include/antidote_utils.hrl
@@ -25,3 +25,38 @@
 -type vectorclock() :: vectorclock:vectorclock().
 -type txid() :: #tx_id{}.
 -type clocksi_payload() :: #clocksi_payload{}.
+
+
+
+%% AntidoteDB
+-type bucket() :: term().
+-record (payload, {key:: key(), type :: type(), op_param, actor :: actor()}).
+
+-record(commit_log_payload, {commit_time :: dc_and_commit_time(),
+    snapshot_time :: snapshot_time()
+}).
+
+-record(update_log_payload, {key :: key(),
+    bucket :: bucket(),
+    type :: type(),
+    op :: op()
+}).
+
+-record(abort_log_payload, {}).
+
+-record(prepare_log_payload, {prepare_time :: non_neg_integer()}).
+
+-type any_log_payload() :: #update_log_payload{} | #commit_log_payload{} | #abort_log_payload{} | #prepare_log_payload{}.
+
+-record(log_operation, {
+    tx_id :: txid(),
+    op_type :: update | prepare | commit | abort | noop,
+    log_payload :: #commit_log_payload{}| #update_log_payload{} | #abort_log_payload{} | #prepare_log_payload{}}).
+-record(op_number, {node :: {node(),dcid()}, global :: non_neg_integer(), local :: non_neg_integer()}).
+
+%% The way records are stored in the log.
+-record(log_record,{
+    version :: non_neg_integer(), %% The version of the log record, for backwards compatability
+    op_number :: #op_number{},
+    bucket_op_number :: #op_number{},
+    log_operation :: #log_operation{}}).
